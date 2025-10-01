ac591cb08edd851cbeeef47667034873a187e150
Kaushik Balaji.M.S <kaushikbalaji0609@outlook.com>
2025-10-02 00:10:25 +0530

    Delete lab1 directory

--- lab1/full_adder.v
+++
@@ -1,7 +0,0 @@
-module full_adder(input a, input b, input cin, output sum, output cout);
-  //assign {cout, sum} = a + b + cin;
-  assign sum  = a ^ b ^ cin;
-  assign cout = (a & b) | (b & cin) | (a & cin);
-endmodule
-
-
